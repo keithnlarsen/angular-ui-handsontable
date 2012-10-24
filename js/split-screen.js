50b4511579b03296f60ecc2d77414e975a5a5444
Keith Larsen <klarsen@marquisalliance.com>
2012-10-23 21:47:45 -0600

    Just a few changes to make this control meet my needs

--- js/split-screen.js
+++ js/split-screen.js
@@ -1,70 +1,147 @@
 function MyCtrl($scope, $filter) {
-  $scope.items = [
-    {id: 1, name: {first: "Marcin", last: "Warpechowski"}, address: "Schellingstr. 58, Muenchen", isActive: 'Yes', "Product": {
-      "Description": "Big Mac",
-      "Options": [
-        {"Description": "Big Mac", "Image": "bigmac.png", Pick$: null},
-        {"Description": "Big Mac & Co", "Image": "bigmacco.png", Pick$: null}
-      ]}},
-    {id: 2, name: {first: "John", last: "Irving"}, address: "Chengdu Road, Wanhua, Taipei 108", isActive: 'Yes', "Product": {
-      "Description": "Fried Potatoes",
-      "Options": [
-        {"Description": "Fried Potatoes", "Image": "", Pick$: null},
-        {"Description": "Fried Onions", "Image": "", Pick$: null}
-      ]}},
-    {id: 3, name: {first: "Jeremy", last: "Springsteen"}, address: "4 New York Plaza, New York, NY 10004", isActive: 'Yes', "Product": {
-      "Description": "McRoyal",
-      "Options": [
-        {"Description": "McRoyal", "Image": "", Pick$: null},
-        {"Description": "McRoyal with Cheese", "Image": "", Pick$: null}
-      ]}}
-  ];
+    $scope.dropDownOptions = [ "Option 1", "Option 2","Option 3","Option 4"];
+  
+    $scope.items = [{
+        id: 1,
+        name: {
+            first: "Marcin",
+            last: "Warpechowski"
+        },
+        address: "Schellingstr. 58, Muenchen",
+        isActive: 'Yes',
+        Product: {
+            Description: "Big Mac"
+        }
+    },
+    {
+        id: 2,
+        name: {
+            first: "John",
+            last: "Irving"
+        },
+        address: "Chengdu Road, Wanhua, Taipei 108",
+        isActive: 'Yes',
+        Product: {
+            Description: "Fried Potatoes"
+        }
+    },
+    {
+        id: 3,
+        name: {
+            first: "John",
+            last: "Irving"
+        },
+        address: "Chengdu Road, Wanhua, Taipei 108",
+        isActive: 'Yes',
+        Product: {
+            Description: "Fried Potatoes"
+        }
+    },
+    {
+        id: 4,
+        name: {
+            first: "John",
+            last: "Irving"
+        },
+        address: "Chengdu Road, Wanhua, Taipei 108",
+        isActive: 'Yes',
+        Product: {
+            Description: "Fried Potatoes"
+        }
+    },
+    {
+        id: 5,
+        name: {
+            first: "John",
+            last: "Irving"
+        },
+        address: "Chengdu Road, Wanhua, Taipei 108",
+        isActive: 'Yes',
+        Product: {
+            Description: "Fried Potatoes"
+        }
+    },
+    {
+        id: 6,
+        name: {
+            first: "John",
+            last: "Irving"
+        },
+        address: "Chengdu Road, Wanhua, Taipei 108",
+        isActive: 'Yes',
+        Product: {
+            Description: "Fried Potatoes"
+        }
+    },
+    {
+        id: 7,
+        name: {
+            first: "John",
+            last: "Irving"
+        },
+        address: "Chengdu Road, Wanhua, Taipei 108",
+        isActive: 'Yes',
+        Product: {
+            Description: "Fried Potatoes"
+        }
+    },
+    {
+        id: 8,
+        name: {
+            first: "Jeremy",
+            last: "Springsteen"
+        },
+        address: "4 New York Plaza, New York, NY 10004",
+        isActive: 'Yes',
+        Product: {
+            Description: "McRoyal"
+        }
+    }];
 
-  $scope.dumpItems = function () {
-    console.log("dump items", $scope.items);
-  };
+    $scope.dumpItems = function () {
+        console.log("dump items", $scope.items);
+    };
 
-  $scope.getOptions = function (options) {
-    var out = [];
-    if (options !== null && typeof options === 'object' && options.length) {
-      for (var i = 0, ilen = options.length; i < ilen; i++) {
-        out.push(options[i].Description);
-      }
-    }
-    return out;
-  };
+    $scope.getOptions = function (options) {
+        var out = [];
+        if (options !== null && typeof options === 'object' && options.length) {
+            for (var i = 0, ilen = options.length; i < ilen; i++) {
+               out.push(options[i].Description);
+            }
+        }
+        return out;
+    };
 
-  $scope.grayedOut = {
-    renderer: function (instance, td, row, col, prop, value, cellProperties) {
-      Handsontable.TextCell.renderer.apply(this, arguments);
-      td.style.color = '#777';
-    }
-  };
+    $scope.grayedOut = {
+        renderer: function (instance, td, row, col, prop, value, cellProperties) {
+            Handsontable.TextCell.renderer.apply(this, arguments);
+            td.style.color = '#777';
+        }
+    };
 
-  /**
-   * Filter
-   */
-
-  $scope.$watch('query', function (newVal, oldVal) {
-    $scope.filteredItems = $filter('filter')($scope.items, $scope.query);
-    $scope.dataChange = !$scope.dataChange;
-  });
-  $scope.filteredItems = $scope.items;
-
-  /**
-   * Selection
-   */
-
-  $scope.currentSelection = "None";
-
-  $scope.$on('datagridSelection', function (scope, $container, r, p, r2, p2) {
-    var ht = $container.data('handsontable');
-    var str = "row '" + r + "' col '" + ht.propToCol(p) + "' (prop '" + p + "')";
-    if (r !== r2 && p !== p2) {
-      str = "From " + str + " to row '" + r2 + "' col '" + ht.propToCol(p2) + "' (prop '" + p2 + "')";
-    }
-    $scope.$apply(function () {
-      $scope.currentSelection = str;
+    /**
+    * Filter
+    */
+    $scope.$watch('query', function (newVal, oldVal) {
+        $scope.filteredItems = $filter('filter')($scope.items, $scope.query);
+        $scope.dataChange = !$scope.dataChange;
     });
-  });
+    $scope.filteredItems = $scope.items;
+
+    /**
+    * Selection
+    */
+
+    $scope.currentSelection = "None";
+
+    $scope.$on('datagridSelection', function (scope, $container, r, p, r2, p2) {
+        var ht = $container.data('handsontable');
+        var str = "row '" + r + "' col '" + ht.propToCol(p) + "' (prop '" + p + "')";
+        if (r !== r2 && p !== p2) {
+            str = "From " + str + " to row '" + r2 + "' col '" + ht.propToCol(p2) + "' (prop '" + p2 + "')";
+        }
+        $scope.$apply(function () {
+            $scope.currentSelection = str;
+        });
+    });
 }
